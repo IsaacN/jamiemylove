ffa882576daf94fb28e799db7be167e5b48e8fbb
Isaac Nankavill <me@isaacnankavill.com>
2018-09-16 21:00:04 +1000

    Initial

---
+++ head.php
@@ -0,0 +1,89 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Edit title tag</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/line-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/theme.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all"/>
+        <!--[if gte IE 9]>
+        	<link rel="stylesheet" type="text/css" href="css/ie9.css" />
+		<![endif]-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700%7CRaleway:700' rel='stylesheet' type='text/css'>
+        <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+    	<div class="loader">
+    		<div class="spinner">
+			  <div class="double-bounce1"></div>
+			  <div class="double-bounce2"></div>
+			</div>
+    	</div>
+				
+		<div class="nav-container">
+						
+			<nav class="top-bar overlay-bar offscreen-menu">
+				<div class="container">
+				
+					<div class="row nav-menu clearfix">
+						<div class="col-xs-10 col-xs-push-1">
+							<img class="logo logo-light" alt="Logo" src="img/logo-light.png">
+							<img class="logo logo-dark" alt="Logo" src="img/logo-dark.png">
+						</div>
+						
+						<div class="col-xs-2 text-right pull-right">
+							<div class="offscreen-toggle">
+								<i class="icon icon_menu text-white"></i>
+							</div>
+						</div>
+					</div>
+					
+					
+					</div>
+					
+				</div>
+				
+				<div class="offscreen-container">
+					<img alt="logo" class="logo" src="img/logo-dark.png" style="padding-bottom: 20px;">
+					<ul class="menu">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Portfolio</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>	
+					<ul class="social-icons">
+						<li>
+							<a href="#">
+								<i class="icon social_twitter"></i>
+							</a>
+						</li>
+				
+						<li>
+							<a href="#">
+								<i class="icon social_facebook"></i>
+							</a>
+						</li>
+				
+						<li>
+							<a href="#">
+								<i class="icon social_instagram"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		
+		</div>
---
+++ index.php
@@ -0,0 +1,79 @@
+	<?php include('head.php');?>	
+		<div class="main-container">
+		<section class="hero-slider">
+				<ul class="slides">
+					<li class="overlay">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" src="img/hero4.jpg">
+						</div>
+						
+						<div class="video-wrapper" styly="min-height: 100%;">
+							<video autoplay="" muted="" loop="">
+								<source type="video/mp4" src="video/film-web.mp4">
+							</video>
+						</div>
+						
+						<div class="container align-vertical">
+							<div class="row">
+								<div class="col-xs-12 custom-hero-text">
+									<h1 class="text-white splice-title">splice so nice</h1>
+									<h2 class='splice-title'> SUMMER 2018 COLLECTION </h2>
+								</div>
+							</div>
+						</div>
+					</li>
+					
+					
+		
+				</ul>
+			</section>
+			
+			<section class="hero-slider">
+				<ul class="slides">
+					
+					
+					<li class="overlay">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" src="img/explore_campaign_hero.jpg">
+						</div>
+						
+						<div class="container align-vertical">
+							<div class="row">
+								<div class="col-md-6 col-sm-9">
+									<h1 class="text-white">Get ready for the launch of our amazing new template Pivot, You will be awed.</h1>
+									<a target="_blank" href="http://www.mediumra.re/pivot/variant/builder.html" class="btn btn-primary btn-white">Customize Pivot</a>
+									<a href="#" class="btn btn-primary btn-filled">Purchase Pivot</a>
+								</div>
+							</div>
+						</div>
+					</li>
+		
+				</ul>
+			</section>
+			
+			<section class="hero-slider">
+				<ul class="slides">
+					
+					
+					<li class="overlay">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" src="img/jamies_story_hero.jpg">
+						</div>
+						
+						<div class="container align-vertical">
+							<div class="row">
+								<div class="col-md-6 col-sm-9">
+									<h1 class="text-white">Get ready for the launch of our amazing new template Pivot, You will be awed.</h1>
+									<a target="_blank" href="http://www.mediumra.re/pivot/variant/builder.html" class="btn btn-primary btn-white">Customize Pivot</a>
+									<a href="#" class="btn btn-primary btn-filled">Purchase Pivot</a>
+								</div>
+							</div>
+						</div>
+					</li>
+		
+				</ul>
+			</section>
+		</div>
+			<?php include('foot.php');?>	
+
+				
